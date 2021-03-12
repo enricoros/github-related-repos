3841c8557fb603cafe7c16640c679d5e5d90282d
Enrico Ros <enrico.ros@gmail.com>
2021-03-12 00:28:31 -0800

    frontend: test build script

---
+++ build-frontend.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# change this to specify where this application will be served from
+# it not in a parameter yet for risk management
+INSTALL_DIR="/srv/com.githubkpis/static/"
+
+echo "Building the Frontend with a clean docker create-react-app build,"
+echo " and installing on $INSTALL_DIR. Edit this script to change."
+
+# Uncomment to refresh
+git pull || return
+
+# Build
+docker build -f Dockerfile-frontend -t githubkpis-frontend .
+
+# Install
+mkdir -p "$INSTALL_DIR"
+#rm -fr "$INSTALL_DIR"
+# instantiate a container and extract the files into the installation directory
+docker create -ti --name githubkpis-frontend-dummy githubkpis-frontend:latest bash
+docker cp githubkpis-frontend-dummy:/app/build "$INSTALL_DIR"
+docker rm -f githubkpis-frontend-dummy
+# verify the files to be present
+ls -d "$INSTALL_DIR"
+ls -l "$INSTALL_DIR"
